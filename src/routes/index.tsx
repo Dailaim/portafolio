d0a68542881565526bdd5ff29b601c488b6d0cc5
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-01 12:56:02 -0500

    fix ceo icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -188,10 +188,10 @@ const Resume = component$(() => {
         <span class="ml-3">Work</span>
       </h2>
       <ol class="mt-6 space-y-4">
-        {resume.map(({ Logo, ...role }, roleIndex) => (
+        {resume.map(({ Logo, ...Logo }, roleIndex) => (
           <li key={roleIndex} class="flex gap-4">
             <div class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Logo class="h-7 w-7" />
+              <Logo class="h-7 w-7" alt={`${Logo.company} Icon`} />
             </div>
             <dl class="flex flex-auto flex-wrap gap-x-2">
               <dt class="sr-only">Company</dt>
