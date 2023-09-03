c9c25953193e25829f38f7488c7b49fe4185c0f9
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-02 20:05:26 -0500

    create one more time

---
+++ src/routes/one-more-time/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container";
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <Container>
+      <div class="mt-12">One more time</div>
+    </Container>
+  );
+});
