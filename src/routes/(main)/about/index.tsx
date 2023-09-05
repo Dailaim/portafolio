9e1a40850b19eb7209c9972a2796f08d46e0574f
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-05 17:53:35 -0500

    update about

--- src/routes/(main)/about/index.tsx
+++ src/routes/(main)/about/index.tsx
@@ -86,30 +86,31 @@ export default component$(() => {
           </h1>
           <div class="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              As a developer, I see beyond just lines of code, finding beauty in
+              the finer details, much like the flowers that inspire me every
+              day. Born and raised in Colombia, I've melded my passion for
+              technology with my love for nature, leading to projects that
+              bridge these two worlds. Every creation stands as a testament to
+              my commitment to innovation and my aspiration to contribute a
+              unique touch to the vast digital cosmos.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              From my early days in programming, I've been enamored with the
+              potential of turning abstract ideas into tangible realities. This
+              enthusiasm has drawn me deep into the realm of AI-driven photo
+              editing, where technical precision meets artistry.
             </p>
-            <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+            <p class="text-justify">
+              <span class="dark:bg-violet-800 dark:text-purple-300 bg-teal-200 text-teal-900 px-2 rounded-md">
+                With two years of experience
+              </span>{" "}
+              under my belt, my confidence has only grown. No challenge seems
+              insurmountable, and I believe wholeheartedly that I can take on
+              any task that comes my way. My code isn't just a set of machine
+              instructions; it's an extension of who I am, my interests, and how
+              I perceive the world. Each project is a further step in my
+              journey, and through my open-source endeavors, I invite others to
+              join me—to explore, contribute, and keep learning together.
             </p>
           </div>
         </div>
