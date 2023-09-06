23e4a0b12dca02c275ba2afbb141da27153f59a4
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-05 20:28:56 -0500

    add animation

--- src/routes/(main)/articles/index.tsx
+++ src/routes/(main)/articles/index.tsx
@@ -7,18 +7,41 @@ import {
 } from "@/components/Card";
 import { SimpleLayout } from "@/components/SimpleLayout";
 import { formatDate } from "@/lib/formatDate";
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 
 import articles from "@/routes/articles.json";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { animate, stagger } from "motion";
 
 const Article = component$<{
   description: string;
   title: string;
   date: string;
   slug: string;
-}>(({ date, description, slug, title }) => {
+  delay: number;
+}>(({ date, description, slug, title, delay }) => {
+  const divSig = useSignal<HTMLElement>();
+
+  useVisibleTask$(() => {
+    if (!divSig.value) return;
+    animate(
+      divSig.value,
+      {
+        opacity: 1,
+        transform: "translate3d(0, 20%, 0)",
+      },
+      { delay: stagger(delay * 100) }
+    );
+  });
+
   return (
-    <article class="md:grid md:grid-cols-4 md:items-baseline">
+    <article
+      ref={divSig}
+      style={{
+        opacity: 0,
+      }}
+      class={["md:grid md:grid-cols-4 md:items-baseline "]}
+    >
       <Card class="md:col-span-3">
         <CardTitle href={`/articles/${slug}`}>{title}</CardTitle>
         <CardEyebrow as="time" dateTime={date} class="md:hidden" decorate>
@@ -34,25 +57,34 @@ const Article = component$<{
   );
 });
 
-export const metadata = {
-  title: "Articles",
-  description:
-    "All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.",
-};
-
 export default component$(() => {
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Floral Codes & Digital Brushes: Tales of Software, AI-driven Art, and Life's Vibrant Palette."
+      intro="My reflections on weaving code with artistry, leading digital endeavors, crafting product experiences, and the rhythmic dance of life, all presented in sequence."
     >
-      <div class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div class="flex max-w-3xl flex-col space-y-16">
-          {articles.map((article) => (
-            <Article key={article.slug} {...article} />
+      <div
+        class={[
+          "md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40",
+        ]}
+      >
+        <div class={["flex max-w-3xl flex-col space-y-16 "]}>
+          {articles.map((article, index) => (
+            <Article key={article.slug} {...article} delay={index + 1} />
           ))}
         </div>
       </div>
     </SimpleLayout>
   );
 });
+
+export const head: DocumentHead = {
+  meta: [
+    {
+      name: "description",
+      content:
+        "Delving into the fusion of coding artistry, AI-driven imagery, leadership insights, and life's vibrant hues, from one chapter to the next.",
+    },
+  ],
+  title: "Leonardo's Chronicles",
+};
